0285e6041c2f018c6e6f05a9bae14315081aa80c
Mounir Benzid <m.benzid@douglas.de>
2023-01-26 00:13:33 +0100

    some Payload

---
+++ complete/src/main/java/blue/Payload.java
@@ -0,0 +1,23 @@
+package blue;
+
+public class Payload {
+	public Payload() {
+	}
+
+    private String payload;
+
+    public Payload(String payload) {
+        this.payload = payload;
+    }
+
+    public String getPayload() {
+        return payload;
+    }
+
+    @Override
+    public String toString() {
+        return "Payload{" +
+            "message='" + payload + '\'' +
+            '}';
+    }
+}
