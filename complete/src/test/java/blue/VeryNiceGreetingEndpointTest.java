dd20867fff2d0f49be8de607ed8ceafeb165e712
Mounir Benzid <m.benzid@douglas.de>
2023-01-29 01:56:42 +0100

    cmnts

--- complete/src/test/java/blue/VeryNiceGreetingEndpointTest.java
+++ complete/src/test/java/blue/VeryNiceGreetingEndpointTest.java
@@ -19,22 +19,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class VeryNiceGreetingEndpointTest {
 
-	// Spring Boot will create a `WebTestClient` for you,
-	// already configure and ready to issue requests against "localhost:RANDOM_PORT"
-	@Autowired
-	private WebTestClient webTestClient;
+    // Spring Boot will create a `WebTestClient` for you,
+    // already configure and ready to issue requests against "localhost:RANDOM_PORT"
+    @Autowired
+    private WebTestClient webTestClient;
 
-	@Test
-	public void testHello() {
-		webTestClient
-			// Create a GET request to test an endpoint
-			.get().uri("/say/hello/Fred")
-			.accept(MediaType.APPLICATION_JSON)
-			.exchange()
-			// and use the dedicated DSL to test assertions against the response
-			.expectStatus().isOk()
-			.expectBody(PayloadDTO.class).value(payload -> {
-				assertThat(payload.getData()).isEqualTo("Hi Fred!");
-		});
-	}
+    @Test
+    public void testHello() {
+        webTestClient
+            // Create a GET request to test an endpoint
+            .get().uri("/say/hello/Fred")
+            .accept(MediaType.APPLICATION_JSON)
+            .exchange()
+            // and use the dedicated DSL to test assertions against the response
+            .expectStatus().isOk()
+            .expectBody(PayloadDTO.class).value(payload -> {
+                assertThat(payload.getData()).isEqualTo("Hi Fred!");
+            });
+    }
 }
